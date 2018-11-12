60f5e462fa7717ba9e62efff8a2285ffe7fb9350
rajanenu <36930674+rajanenu@users.noreply.github.com>
2018-11-12 12:10:11 +0530

    Add files via upload

--- BasicsPython.py
+++ BasicsPython.py
@@ -1,3 +1,16 @@
 print("Hello,World")
-a=2+8
-print(a)
+sum=2+8
+print(sum)
+flag=True
+#if else condition
+if flag:
+    print ("True")
+    flag=False
+else:
+    print ("False")
+#For loop
+for val in [1,2,3,4]:
+    print (val)
+#range function
+for val in range(5):
+    print (val)
